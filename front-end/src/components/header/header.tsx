36543d6903d9739cbdd2f714d6273c73a0a73c80
kauanAg <117299925+KauanAg-devs@users.noreply.github.com>
2025-06-17 15:03:21 +0000

    feat: header completed

--- front-end/src/components/header/header.tsx
+++ front-end/src/components/header/header.tsx
@@ -2,8 +2,8 @@ import { HeaderProps } from '@/types/header'
 
 export default  function Header({ children }: HeaderProps){
   return (
-    <>
+    <header className='bg-white'>
     {children}
-    </>
+    </header>
   )
 }
---
+++ front-end/src/types/menu-props.ts
@@ -0,0 +1,9 @@
+export type MenuProps = {
+    showCompanyProducts: boolean
+    setShowCompanyProducts: React.Dispatch<React.SetStateAction<boolean>>
+
+}
+export type MenuProps = {
+    showMobileProducts: boolean
+    setShowMobileProducts: React.Dispatch<React.SetStateAction<boolean>>
+}
