eafcb203ceaab40687822ec273816626c0d83a3e
Kauan Barcelos <9919753-KauanBarcelos@users.noreply.replit.com>
2025-06-25 17:29:07 +0000

    refactor: user profile typing for explicitness coding

--- front-end/src/types/user-profile.ts
+++ front-end/src/types/user-profile.ts
@@ -1,14 +1,38 @@
+import { ProductVariant } from "./product";
+
+export type UserRole = 'admin' | 'seller' | 'user'
+
 export type UserProfile = {
   name: string | null;
   email: string;
   phone: string | null;
   profileImage: string | ArrayBuffer | null;
-
+  memberSince: string;
+  
+  role: UserRole[]
+  
   addressData?: {
     address: string;
     city: string;
     state: string;
     zipCode: string;
     complement: string;
+  };
+
+  userPaymentMethods?: {
+    cardName: string;
+    cardNumber: string;
+    cardType: string;
+    cardCvv: string;
+    expiry: string;
+    isDefault: boolean;
+  }[]
+
+  purchases?: {
+    variants: ProductVariant[];
+  };
+
+  sales?: {
+    variants: ProductVariant[];
   };
 };
